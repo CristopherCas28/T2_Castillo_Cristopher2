0914957197f5c1dd44633017bae422bd749efb96
CristopherCas28 <cristopher242002@gmail.com>
2024-05-28 14:26:02 -0500

    Commit1

---
+++ T2_Castillo_Cristopher/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using T2_Castillo_Cristopher.Models;
+
+namespace T2_Apaza_Felix.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        //Distribuidores Peru
+        public IActionResult DistribuidorPeru()
+        {
+            return View();
+        }
+
+        //Distribuidores Mundo
+        public IActionResult DistribuidorMundo()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
